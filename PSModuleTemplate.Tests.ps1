e6b461074e9065480378c3a7d4faa527aedb8463
Brian Lalonde <brianary@gmail.com>
2020-01-18 18:58:57 -0800

    Improve module tests

--- PSModuleTemplate.Tests.ps1
+++ PSModuleTemplate.Tests.ps1
@@ -1,15 +1,26 @@
 # Pester tests, see https://github.com/Pester/Pester/wiki
 $envPath = $env:Path # avoid testing the wrong cmdlets
-Import-Module (Resolve-Path ./src/*/bin/Debug/*/*.psd1) -vb
-Describe 'PSModuleTemplate' {
-    Context 'PSModuleTemplate module' {
-        It "Given the PSModuleTemplate module, it should have a nonzero version" {
-            $m = Get-Module PSModuleTemplate
-            $m.Version |Should -Not -Be $null
-            $m.Version.Major |Should -BeGreaterThan 0
+$module = Import-Module (Resolve-Path ./src/*/bin/Debug/*/*.psd1) -PassThru -vb
+Import-LocalizedData -BindingVariable manifest -BaseDirectory ./src/* -FileName (Split-Path $PWD -Leaf)
+Describe $module.Name {
+    Context "$($module.Name) module" -Tag Module {
+        It "Given the module, the version should match the manifest version" {
+            $module.Version |Should -BeExactly $manifest.ModuleVersion
         }
+		It "Given the module, the DLL file version should match the manifest version" {
+            (Get-Item "$($module.ModuleBase)\$($module.Name).dll").VersionInfo.FileVersionRaw |
+                Should -BeLike "$($manifest.ModuleVersion)*"
+		}
+		It "Given the module, the DLL product version should match the manifest version" {
+            (Get-Item "$($module.ModuleBase)\$($module.Name).dll").VersionInfo.ProductVersionRaw |
+                Should -BeLike "$($manifest.ModuleVersion)*"
+		} -Pending
+		It "Given the module, the DLL should have a valid semantic product version" {
+			$v = (Get-Item "$($module.ModuleBase)\$($module.Name).dll").VersionInfo.ProductVersion
+			[semver]::TryParse($v, [ref]$null) |Should -BeTrue
+		} -Pending
     }
-    Context 'Get-Foo cmdlet' {
+    Context 'Get-Foo cmdlet' -Tag Cmdlet,Get-Foo {
         It "Given a name '<Name>', '<Expected>' should be returned." -TestCases @(
             @{ Name = 'Hello, world'; Expected = 'Hello, world' }
             @{ Name = 'Zaphod'; Expected = 'Zaphod' }
