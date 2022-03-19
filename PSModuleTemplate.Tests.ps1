54c9e7e4216cbc183f738cbef6119bba00beb310
Brian Lalonde <brianary@gmail.com>
2022-03-18 19:52:01 -0700

    Finish cleanup

--- PSModuleTemplate.Tests.ps1
+++ PSModuleTemplate.Tests.ps1
@@ -3,33 +3,32 @@ Import-LocalizedData -BindingVariable manifest -BaseDirectory ./src/* -FileName 
 $psd1 = Resolve-Path ./src/*/bin/Debug/*/*.psd1
 if(1 -lt ($psd1 |Measure-Object).Count) {throw "Too many module binaries found: $psd1"}
 $module = Import-Module "$psd1" -PassThru -vb
+
 Describe $module.Name {
-    Context "$($module.Name) module" -Tag Module {
-        It "Given the module, the version should match the manifest version" {
-            $module.Version |Should -BeExactly $manifest.ModuleVersion
-        }
+	Context "$($module.Name) module" -Tag Module {
+		It "Given the module, the version should match the manifest version" {
+			$module.Version |Should -BeExactly $manifest.ModuleVersion
+		}
 		It "Given the module, the DLL file version should match the manifest version" {
-            (Get-Item "$($module.ModuleBase)\$($module.Name).dll").VersionInfo.FileVersionRaw |
-                Should -BeLike "$($manifest.ModuleVersion)*"
+			(Get-Item "$($module.ModuleBase)\$($module.Name).dll").VersionInfo.FileVersionRaw |
+				Should -BeLike "$($manifest.ModuleVersion)*"
 		}
 		It "Given the module, the DLL product version should match the manifest version" {
-            (Get-Item "$($module.ModuleBase)\$($module.Name).dll").VersionInfo.ProductVersionRaw |
-                Should -BeLike "$($manifest.ModuleVersion)*"
+			(Get-Item "$($module.ModuleBase)\$($module.Name).dll").VersionInfo.ProductVersionRaw |
+				Should -BeLike "$($manifest.ModuleVersion)*"
 		} -Pending
 		It "Given the module, the DLL should have a valid semantic product version" {
 			$v = (Get-Item "$($module.ModuleBase)\$($module.Name).dll").VersionInfo.ProductVersion
 			[semver]::TryParse($v, [ref]$null) |Should -BeTrue
 		} -Pending
-    }
-    Context 'Get-Foo cmdlet' -Tag Cmdlet,Get-Foo {
-        It "Given a name '<Name>', '<Expected>' should be returned." -TestCases @(
-            @{ Name = 'Hello, world'; Expected = 'Hello, world' }
-            @{ Name = 'Zaphod'; Expected = 'Zaphod' }
-        ) {
-            Param($Name,$Expected)
-            PSModuleTemplate\Get-Foo $Name |Should -BeExactly $Expected
-        }
-    }
+	}
+	Context 'Get-Foo cmdlet' -Tag Cmdlet,Get-Foo {
+		It "Given a name '<Name>', '<Expected>' should be returned." -TestCases @(
+			@{ Name = 'Hello, world'; Expected = 'Hello, world' }
+			@{ Name = 'Zaphod'; Expected = 'Zaphod' }
+		) {
+			Param($Name,$Expected)
+			PSModuleTemplate\Get-Foo $Name |Should -BeExactly $Expected
+		}
+	}
 }.GetNewClosure()
-$env:Path = $envPath
-`
